f2dbab16ce5252c30a1a21adc0b56de35b843419
grapes tree <treeui.old@gmail.com>
2014-01-14 17:57:49 +0800

    Update Select_func.hpp

--- Select_func.hpp
+++ Select_func.hpp
@@ -5,6 +5,7 @@
 #include <list>
 #include <iostream>
 #include <math.h>
+#include <deque>
 using namespace std;
 
 //how to use f(x,y) = h(x,y) + g(x,y)
@@ -17,11 +18,14 @@ int step;
 
 
 double function_h(Base & now){
-	return (abs(source.x-now.x)+abs(source.y-now.y));
+	//return (abs(source.x-now.x)+abs(source.y-now.y));
+	//return sqrt(abs(source.x-now.x)*abs(source.x-now.x)+abs(source.y-now.y)*abs(source.y-now.y));
+	return 0;
 }
 
 double function_g(Base & now){
 	return sqrt(abs(target.x-now.x)*abs(target.x-now.x)+abs(target.y-now.y)*abs(target.y-now.y));
+	//return (target.x-now.x)+(target.y-now.y);
 }
 
 void updata_current(Base & now){
@@ -34,7 +38,7 @@ void updata_current(Base & now){
 
 }
 double function_f(Base & now){
-	updata_current(now);
+	//updata_current(now);
 	return (function_h(now)+function_g(now));
 }
 #if 0
@@ -55,7 +59,7 @@ class Compare
 {
 public:
 	bool operator()(const Base& first,Base& second){
-		if ((first.f_value>second.f_value))
+		if ((first.f_value<second.f_value))
 		{
 			return true;
 		}
@@ -70,7 +74,7 @@ public:
 	Try_Way()
 		:new_map(Map())
 	{
-
+		 new_map.print_map();
 	}
 	void updata_openlist(){
 		for(auto itr =openlist.begin(),end=openlist.end();itr != end;itr++)
@@ -78,7 +82,7 @@ public:
 			itr->f_value= function_f(*itr);
 
 		}
-		std::sort(openlist.begin(),openlist.end(),Compare());
+		std::sort(openlist.begin(),openlist.end(),compare_two_base);
 	}
 
 
@@ -128,49 +132,54 @@ public:
 #endif // 0
 	void Generate_next_step(Base now){
 		Base now_up(now.x,now.y-1);
-		if (new_map.check_out_of_map(now_up))
+		if (new_map.check_out_of_map(now_up)&&(new_map.get_positioin_high(now.x,now.y-1)!=1))
 		{
 			now_up.f_value=function_f(now_up);
 			if (!find_in_openlist(now_up))
 			{
 				if (!find_in_closelist(now_up))
 				{
+					now_up.open_close = true;
 					openlist.push_back(now_up);
 				}
 			}
 		}
 		Base now_down(now.x,now.y+1);
-		if (new_map.check_out_of_map(now_down))
+		if (new_map.check_out_of_map(now_down)&&(new_map.get_positioin_high(now.x,now.y+1)!=1))
 		{
 			now_down.f_value=function_f(now_down);
 			if (!find_in_openlist(now_down))
 			{
 				if (!find_in_closelist(now_down))
 				{
+					now_down.open_close = true;
 					openlist.push_back(now_down);
 				}
 			}
 		}
 		Base now_left(now.x-1,now.y);
-		if (new_map.check_out_of_map(now_left))
+		if (new_map.check_out_of_map(now_left)&&(new_map.get_positioin_high(now.x-1,now.y)!=1))
 		{
 			now_left.f_value=function_f(now_left);
 			if (!find_in_openlist(now_left))
 			{
 				if (!find_in_closelist(now_left))
 				{
+					now_left.open_close = true;
 					openlist.push_back(now_left);
 				}
 			}
 		}
 		Base now_right(now.x+1,now.y);
-		if (new_map.check_out_of_map(now_right))
+		if (new_map.check_out_of_map(now_right)&&(new_map.get_positioin_high(now.x+1,now.y)!=1))
 		{
+			
 			now_right.f_value=function_f(now_right);
 			if (!find_in_openlist(now_right))
 			{
 				if (!find_in_closelist(now_right))
 				{
+					now_right.open_close = true;
 					openlist.push_back(now_right);
 				}
 			}
@@ -219,20 +228,33 @@ public:
 				closelist.push_back(current);
 			}
 #endif // 0
+			
 			closelist.push_back(current);
 			Generate_next_step(current);
 			updata_openlist();
-			current = *(openlist.begin());
+			if (openlist.empty())
+			{
+				cout<<"no effective node to step in!"<<endl; ;
+				break;
+			} 
+			else
+			{
+				current = *(openlist.begin());
+			}
+			
+			current.open_close = false;
 			openlist.clear();
 		}
+		closelist.push_back(target);
+		cout<<closelist.size()<<endl;
 
 	}
 
 
 protected:
 private:
-	std::vector<Base> openlist;//准备探索的路线
-	std::vector<Base> closelist;//已经确定的路线
+	std::deque<Base> openlist;//准备探索的路线
+	std::deque<Base> closelist;//已经确定的路线
 	Map new_map;//主要起限定作用
 };
 
